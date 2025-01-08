482caf4aeefa04fd9722398c95ecb1f1f4bc66cd
Sahil Sanjay Patil <97878428+SahilsPatil@users.noreply.github.com>
2025-01-08 14:01:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Create a directory for ffmpeg binary in /tmp
-mkdir -p /tmp/ffmpeg
+# # Create a directory for ffmpeg binary in /tmp
+# mkdir -p /tmp/ffmpeg
 
-# Download ffmpeg binary for Linux (static build)
-wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz -P /tmp/ffmpeg
+# # Download ffmpeg binary for Linux (static build)
+# wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz -P /tmp/ffmpeg
 
-# Extract the tar file
-tar -xf /tmp/ffmpeg/ffmpeg-release-i686-static.tar.xz -C /tmp/ffmpeg
+# # Extract the tar file
+# tar -xf /tmp/ffmpeg/ffmpeg-release-i686-static.tar.xz -C /tmp/ffmpeg
 
-# Remove the tar file after extraction
-rm /tmp/ffmpeg/ffmpeg-release-i686-static.tar.xz
+# # Remove the tar file after extraction
+# rm /tmp/ffmpeg/ffmpeg-release-i686-static.tar.xz
 
 pip install -r requirements.txt
