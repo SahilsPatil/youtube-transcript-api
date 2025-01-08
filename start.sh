00ac1df3bcb4b94579095b9976f75fc58474cf8c
Sahil Sanjay Patil <97878428+SahilsPatil@users.noreply.github.com>
2025-01-08 18:58:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gunicorn -w 4 -b 0.0.0.0:5000 --timeout 500 app:app
+#gunicorn -w 4 -b 0.0.0.0:5000 --timeout 500 app:app
+gunicorn -w 4 -b 0.0.0.0:5000 --timeout 500 app:app --log-level=debug --access-logfile=- --error-logfile=-
