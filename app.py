6623a7d4123c6bcab0677daa072c74bc1742d23a
Sahil Sanjay Patil <97878428+SahilsPatil@users.noreply.github.com>
2025-01-08 17:48:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def download_audio(video_url):
     ydl_opts = {
         'format': 'm4a/bestaudio/best',  # The best audio version in m4a format
         'outtmpl': '%(id)s.%(ext)s',  # The output name should be the id followed by the extension
-        'proxy': 'dwuojzgn:wx0b3xey9xxm@173.0.9.70:5653',
+        'proxy': 'dwuojzgn:wx0b3xey9xxm@64.137.42.112:5157',
         'http_headers': {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
         },
